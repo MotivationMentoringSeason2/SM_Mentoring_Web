2ea633cdabb722082a4379abb63d1a8ad11e073b
wkdtndgns <wkdtndgns@naver.com>
2018-09-13 03:17:18 +0900

    공지사항 구조 작업 완료

---
+++ src/component/notice_page/NoticeForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class NoticeForm extends Component {
+	componentWillMount() {
+ 
+  }
+
+  render() {
+
+    
+    return (
+      <div>
+        공지사항
+        <br/>
+        컴포넌트
+        연결 확인 
+      </div>
+    );
+  }
+}
+
---
+++ src/page/notice_page/NoticePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NoticeFormContainer} from "../../container";
+
+const NoticePage = () => (
+    <div>
+
+        <br/><br/>
+        <NoticeFormContainer />
+    </div>
+);
+export default NoticePage;
