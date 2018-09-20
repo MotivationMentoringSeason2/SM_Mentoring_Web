9b702bc49180d859ec831fee8a9fdd1e488c9b86
tails5555 <tails5555@naver.com>
2018-09-20 15:24:16 +0900

    Update Detail Title List Component 세부문 저장, 수정, 삭제하는 기능을 Redux 입혀서 작업 완료 해뒀음.

--- src/container/DetailTitleListContainer.js
+++ src/container/DetailTitleListContainer.js
@@ -1,12 +1,18 @@
 import {DetailTitleList} from "../component/intro_detail_edit_page";
 import {connect} from 'react-redux';
 import {
-    adminLoadDetailList, adminLoadDetailListSuccess, adminLoadDetailListFailure, resetAdminLoadDetailList
+    adminLoadDetailList, adminLoadDetailListSuccess, adminLoadDetailListFailure, resetAdminLoadDetailList,
+    adminUpdateDetailContext, adminCreateDetailContextFailure, resetAdminSaveDetailContext, resetAdminRemoveDetailMulti,
+    adminRemoveDetailMultiFailure, adminUpdateDetailContextSuccess, adminCreateDetailContext,
+    adminUpdateDetailContextFailure, adminCreateDetailContextSuccess, adminRemoveDetailMulti,
+    adminRemoveDetailMultiSuccess
 } from "../action/action_intro";
 
 function mapStateToProps(state){
     return {
         detailList : state.intro.detailList,
+        saveStatus : state.intro.saveStatus,
+        deleteStatus : state.intro.deleteStatus,
         accessAccount : state.account.accessAccount
     }
 }
@@ -19,7 +25,27 @@ const mapDispatchToProps = (dispatch) => {
             else
                 dispatch(adminLoadDetailListFailure(response.payload));
         }),
-        resetFetchDetailList : () => dispatch(resetAdminLoadDetailList())
+        resetFetchDetailList : () => dispatch(resetAdminLoadDetailList()),
+        executeUpdateDetail : (detailForm, writer) => dispatch(adminUpdateDetailContext(detailForm, writer)).then((response) => {
+            if(response.payload && response.payload.status === 200)
+                dispatch(adminUpdateDetailContextSuccess(response.payload));
+            else
+                dispatch(adminUpdateDetailContextFailure(response.payload));
+        }),
+        executeCreateDetail : (detailForm, introId, writer) => dispatch(adminCreateDetailContext(detailForm, introId, writer)).then((response) => {
+            if(response.payload && response.payload.status === 200)
+                dispatch(adminCreateDetailContextSuccess(response.payload));
+            else
+                dispatch(adminCreateDetailContextFailure(response.payload));
+        }),
+        resetExecuteSaveDetail : () => dispatch(resetAdminSaveDetailContext()),
+        executeRemoveDetails : (ids) => dispatch(adminRemoveDetailMulti(ids)).then((response) => {
+            if(response.payload && response.payload.status === 200)
+                dispatch(adminRemoveDetailMultiSuccess(response.payload));
+            else
+                dispatch(adminRemoveDetailMultiFailure(response.payload));
+        }),
+        resetExecuteRemoveDetails : () => dispatch(resetAdminRemoveDetailMulti())
     }
 }
 
