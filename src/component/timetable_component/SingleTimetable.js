5404a528ea2827d084a60135bb7c5fe1e4993ef6
tails5555 <tails5555@naver.com>
2018-09-23 21:53:14 +0900

    Update Mento Application Form - Add Redux Actions 멘토 신청 기능에 대해 Redux 로 설정 완료.

--- src/component/timetable_component/SingleTimetable.js
+++ src/component/timetable_component/SingleTimetable.js
@@ -34,11 +34,11 @@ class SingleTimetable extends Component{
                 </div>
                 <br/>
                 <div className="w3-padding-16">
-                    <button id="MON" className="w3-button w3-round-large w3-red" onClick={this.handleClick.bind(this)}>월요일</button>
-                    <button id="TUE" className="w3-button w3-round-large w3-orange" onClick={this.handleClick.bind(this)}>화요일</button>
-                    <button id="WED" className="w3-button w3-round-large w3-yellow" onClick={this.handleClick.bind(this)}>수요일</button>
-                    <button id="THU" className="w3-button w3-round-large w3-green" onClick={this.handleClick.bind(this)}>목요일</button>
-                    <button id="FRI" className="w3-button w3-round-large w3-blue" onClick={this.handleClick.bind(this)}>금요일</button>
+                    <button id="MON" type="button" className="w3-button w3-round-large w3-red" onClick={this.handleClick.bind(this)}>월요일</button>
+                    <button id="TUE" type="button" className="w3-button w3-round-large w3-orange" onClick={this.handleClick.bind(this)}>화요일</button>
+                    <button id="WED" type="button" className="w3-button w3-round-large w3-yellow" onClick={this.handleClick.bind(this)}>수요일</button>
+                    <button id="THU" type="button" className="w3-button w3-round-large w3-green" onClick={this.handleClick.bind(this)}>목요일</button>
+                    <button id="FRI" type="button" className="w3-button w3-round-large w3-blue" onClick={this.handleClick.bind(this)}>금요일</button>
                 </div>
             </Grid>
         )
