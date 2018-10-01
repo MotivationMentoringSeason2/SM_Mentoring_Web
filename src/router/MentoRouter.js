5f99a0411f2e47dba2fa12263bd8d2e5e1a4c5ea
wkdtndgns <wkdtndgns@naver.com>
2018-10-01 19:03:34 +0900

    멘토링 목록 구현

--- src/router/MentoRouter.js
+++ src/router/MentoRouter.js
@@ -23,7 +23,7 @@ import {CardPage} from "../page/card_page";
 import {MentoringReportEditPage} from "../page/mentoring_report_edit_page";
 import MentoringReportFormPage from "../page/mentoring_report_form_page/MentoringReportFormPage";
 import {MentoringOpenViewPage} from "../page/mentoring_open_view_page";
-
+import {MentoringPage} from "../page/mentoring_page";
 const MentoRouter = (props) => {
     return(
         <div>
@@ -89,6 +89,7 @@ const MentoRouter = (props) => {
                 <Route exact path="/mento/report/edit/_refresh" render={() => <Redirect to="/mento/report/edit" />} />
                 <Route exact path="/mento/report/create" component={MentoringReportFormPage} />
                 <Route exact path="/mento/report/update" component={MentoringReportFormPage} />
+                <Route exact path="/application/mento_list" component={MentoringPage} />
             </ScrollToTop>
         </div>
     );
